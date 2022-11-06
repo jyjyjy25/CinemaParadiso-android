435b1deb584059aed09d2719e42bf9dd174d74f2
pwdjnyz11 <jyjyjy7418@google.com>
2022-11-06 21:43:40 +0900

    add like event

--- app/src/main/java/org/techtown/cinemaparadiso/MainActivity.java
+++ app/src/main/java/org/techtown/cinemaparadiso/MainActivity.java
@@ -3,12 +3,93 @@ package org.techtown.cinemaparadiso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button likeButton;
+    Button dislikeButton;
+    TextView likeCountView;
+    TextView dislikeCountView;
+
+    int likeCount = 15;
+    int dislikeCount = 1;
+    boolean likeState = false;
+    boolean dislikeState = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        likeButton = (Button) findViewById(R.id.likeButton);
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if(likeState) {
+                    decrLikeCount();
+                } else {
+                    incrLikeCount();
+                }
+
+                likeState = !likeState;
+            }
+        });
+        likeCountView = (TextView) findViewById(R.id.likeCountView);
+
+        dislikeButton = (Button) findViewById(R.id.dislikeButton);
+        dislikeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(dislikeState) {
+                    decrDislikeCount();
+                } else {
+                    incrDislikeCount();
+                }
+
+                dislikeState = !dislikeState;
+            }
+        });
+        dislikeCountView = (TextView) findViewById(R.id.dislikeCountView);
+
+    }
+
+    public void incrLikeCount() {
+        likeCount += 1;
+        likeCountView.setText(String.valueOf(likeCount));
+
+        likeButton.setBackgroundResource(R.drawable.ic_thumb_up_selected);
+        if (dislikeState) {
+            decrDislikeCount();
+            dislikeState = !dislikeState;
+        }
+    }
+
+    public void decrLikeCount() {
+        likeCount -= 1;
+        likeCountView.setText(String.valueOf(likeCount));
+
+        likeButton.setBackgroundResource(R.drawable.thumbs_up_selector);
+    }
+
+    public void incrDislikeCount() {
+        dislikeCount += 1;
+        dislikeCountView.setText(String.valueOf(dislikeCount));
+
+        dislikeButton.setBackgroundResource(R.drawable.ic_thumb_down_selected);
+        if (likeState){
+            decrLikeCount();
+            likeState = !likeState;
+        }
+    }
+
+    public void decrDislikeCount() {
+        dislikeCount -= 1;
+        dislikeCountView.setText(String.valueOf(dislikeCount));
+
+        dislikeButton.setBackgroundResource(R.drawable.ic_thumb_down);
     }
 }
